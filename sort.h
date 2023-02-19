0c6b626469048796020824c0cbcc62492d3ab91b
Jeffrey7890 <d3v3loperpy@gmail.com>
2023-02-19 22:27:55 +0100

    quick sort using Lomuto partition sheme

--- sort.h
+++ sort.h
@@ -22,6 +22,7 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 /* printing functions */
 void print_array(const int *array, size_t size);
