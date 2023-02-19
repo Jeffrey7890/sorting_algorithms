95bd5372ff66cbf60265a8c25f5a7073642fb67a
Jeffrey7890 <d3v3loperpy@gmail.com>
2023-02-19 19:12:49 +0100

    selection sort completed

--- sort.h
+++ sort.h
@@ -20,6 +20,8 @@ typedef struct listint_s
 
 /* sorting algorithms */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 /* printing functions */
 void print_array(const int *array, size_t size);
